08b6f936bd5d12a144a871629d116b017ac958c6
statusbond <greg@statusbond.com>
2025-02-09 18:17:41 -0800

    Update index.js

--- notifications/index.js
+++ notifications/index.js
@@ -1,1 +1,50 @@
+const { 
+    sendStatusChangeSlack, 
+    sendPriceChangeSlack, 
+    sendOpenHouseSlack 
+} = require('./slack');
 
+const { 
+    sendStatusChangeSMS, 
+    sendPriceChangeSMS, 
+    sendOpenHouseSMS 
+} = require('./twilio');
+
+async function sendStatusChange(listingDetails, oldStatus, newStatus) {
+    try {
+        await Promise.all([
+            sendStatusChangeSlack(listingDetails, oldStatus, newStatus),
+            sendStatusChangeSMS(listingDetails, oldStatus, newStatus)
+        ]);
+    } catch (error) {
+        console.error('Error sending notifications:', error);
+    }
+}
+
+async function sendPriceChange(listingDetails, oldPrice, newPrice) {
+    try {
+        await Promise.all([
+            sendPriceChangeSlack(listingDetails, oldPrice, newPrice),
+            sendPriceChangeSMS(listingDetails, oldPrice, newPrice)
+        ]);
+    } catch (error) {
+        console.error('Error sending notifications:', error);
+    }
+}
+
+async function sendOpenHouse(listingDetails, openHouse) {
+    try {
+        await Promise.all([
+            sendOpenHouseSlack(listingDetails, openHouse),
+            sendOpenHouseSMS(listingDetails, openHouse)
+        ]);
+    } catch (error) {
+        console.error('Error sending notifications:', error);
+    }
+}
+
+module.exports = {
+    sendStatusChange,
+    sendPriceChange,
+    sendOpenHouse
+};
