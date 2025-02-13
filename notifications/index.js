cb7fe830f3850f84afa62695b424154229206521
statusbond <greg@statusbond.com>
2025-02-12 18:46:02 -0800

    Update index.js

--- notifications/index.js
+++ notifications/index.js
@@ -15,7 +15,7 @@ function handleListingChange(listingDetails) {
           text: `🏡 *STATUS CHANGE*\n` + 
                 `${listingDetails.address}\n` +
                 `${listingDetails.price}\n` +
-                `→ ${listingDetails.newStatus}\n` +
+                `${listingDetails.newStatus}\n` +
                 `Listing Agent: ${listingDetails.agentName}\n` +
                 `Cell: ${listingDetails.agentPhone}`
         }
